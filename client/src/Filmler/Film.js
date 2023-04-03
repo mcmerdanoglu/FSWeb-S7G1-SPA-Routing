04bd349d1ba6e902627db3a3acc2c499aa19e21f
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-04-03 18:54:07 +0300

    Ders saatine kadar olan yapılanlar

--- client/src/Filmler/Film.js
+++ client/src/Filmler/Film.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
 
 export default function Film(props) {
   const [movie, setMovie] = useState();
@@ -10,16 +11,16 @@ export default function Film(props) {
   useEffect(() => {
     axios
       .get(`http://localhost:5001/api/filmler/${id}`) // Bu uç noktayı Postman'le çalışın
-      .then(response => {
-          // Bu kısmı log statementlarıyla çalışın
-          // ve burdan gelen response'u 'movie' e aktarın
+      .then((response) => {
+        // Bu kısmı log statementlarıyla çalışın
+        // ve burdan gelen response'u 'movie' e aktarın
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
     // Bu effect her `id ` değiştiğinde çalışmalı
     // Bunu nasıl gerçekleştirebiliriz?
-  }, []);
+  }, [id]);
 
   // Yalnızca esnek görevlere geçtiğinizde burdaki yorum etiketini kaldırın
   // const filmiKaydet = evt => { }
@@ -33,20 +34,22 @@ export default function Film(props) {
   return (
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
+        <NavLink activeClassName="active" to="/filmler/:id">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
           </div>
-        ))}
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map((star) => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </NavLink>
       </div>
       <div className="save-button">Kaydet</div>
     </div>
--- client/src/Filmler/FilmListesi.js
+++ client/src/Filmler/FilmListesi.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
 
 export default function FilmListesi(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <FilmDetayları key={movie.id} movie={movie} />
-      ))}
+      <NavLink activeClassName="active" exact to="/">
+        {props.movies.map((movie) => (
+          <FilmDetayları key={movie.id} movie={movie} />
+        ))}
+      </NavLink>
     </div>
   );
-  }
+}
 
 function FilmDetayları(props) {
   const { title, director, metascore } = props.movie;
 
+  const history = useHistory();
+
+  const moveTo = () => {
+    history.push("/movies/:id");
+  };
+
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={moveTo}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
